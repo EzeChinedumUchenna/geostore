f2eafc53fe3105f8b27df9354b5dbb8460111bd5
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2024-07-29 16:08:46 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,15 @@ ECR_REPOSITORY="637423571998.dkr.ecr.us-east-2.amazonaws.com/seamfixapp"  # Chan
 IMAGE_TAG="latest"
 
 # Authenticate Docker to the ECR registry
-aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REPOSITORY
+#aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REPOSITORY
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 637423571998.dkr.ecr.us-east-2.amazonaws.com
+
+
+
 
 # Pull the latest image
-docker pull $ECR_REPOSITORY:$IMAGE_TAG
+#docker pull $ECR_REPOSITORY:$IMAGE_TAG
+docker pull 637423571998.dkr.ecr.us-east-2.amazonaws.com/geostore-app:$CODEBUILD_BUILD_NUMBER
 
 # Stop and remove the existing container if it exists
 if [ "$(docker ps -q -f name=seamfixapp)" ]; then
@@ -18,5 +23,6 @@ if [ "$(docker ps -q -f name=seamfixapp)" ]; then
 fi
 
 # Run the new container
-docker run -d --restart always -p 9191:9191 --name seamfixapp $ECR_REPOSITORY:$IMAGE_TAG
+docker run -d --restart always -p 9191:9191 --name seamfix-app 637423571998.dkr.ecr.us-east-2.amazonaws.com/geostore-app:$CODEBUILD_BUILD_NUMBER
+#docker run -d --restart always -p 9191:9191 --name seamfixapp $ECR_REPOSITORY:$IMAGE_TAG
 
