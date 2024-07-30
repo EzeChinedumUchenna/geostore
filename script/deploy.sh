9d394b5fcfd693e83465597bb2c60b745cba8143
Eze Chinedum <102483586+EzeChinedumUchenna@users.noreply.github.com>
2024-07-30 04:09:19 +0100

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+BUILD_NUMBER=$(cat /home/ubuntu/app/build_number.txt)
+
 # Variables
 REGION="us-east-2"  # Change to your AWS region
 ECR_REPOSITORY="637423571998.dkr.ecr.us-east-2.amazonaws.com/seamfixapp"  # Change to your ECR repository URI..
@@ -14,15 +17,15 @@ aws ecr get-login-password --region us-east-2 | docker login --username AWS --pa
 
 # Pull the latest image
 #docker pull $ECR_REPOSITORY:$IMAGE_TAG
-docker pull 637423571998.dkr.ecr.us-east-2.amazonaws.com/geostore-app:8
+docker pull 637423571998.dkr.ecr.us-east-2.amazonaws.com/geostore-app:$BUILD_NUMBER
 
 # Stop and remove the existing container if it exists
-if [ "$(docker ps -q -f name=seamfixapp)" ]; then
-    docker stop seamfixapp
-    docker rm seamfixapp
+if [ "$(docker ps -q -f name=seamfix-app)" ]; then
+    docker stop seamfix-app
+    docker rm seamfix-app
 fi
 
 # Run the new container
-docker run -d --restart always -p 9191:9191 --name seamfix-app 637423571998.dkr.ecr.us-east-2.amazonaws.com/geostore-app:8
+docker run -d --restart always -p 9191:9191 --name seamfix-app 637423571998.dkr.ecr.us-east-2.amazonaws.com/geostore-app:$BUILD_NUMBER
 #docker run -d --restart always -p 9191:9191 --name seamfixapp $ECR_REPOSITORY:$IMAGE_TAG
 
